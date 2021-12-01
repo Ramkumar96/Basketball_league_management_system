d8e4b99555c60130b25452ef90c9c6d3265bb0e3
Ram kumar <29110677+Ramkumar96@users.noreply.github.com>
2021-12-02 00:05:56 +0530

    initial commit

---
+++ League_Management_System/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LeagueManagementSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'League_Management_System'
