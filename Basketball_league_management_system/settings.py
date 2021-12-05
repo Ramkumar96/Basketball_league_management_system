d2ae9d4f5bcf7850d6282b5f27dc9e6e5700aab6
Ram kumar <29110677+Ramkumar96@users.noreply.github.com>
2021-12-05 22:26:22 +0530

    Testing screenshots

--- Basketball_league_management_system/settings.py
+++ Basketball_league_management_system/settings.py
@@ -74,13 +74,6 @@ WSGI_APPLICATION = 'Basketball_league_management_system.wsgi.application'
 
 # Database Config
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
